9ab10f5270970f2d2112a21f8f63d55b22540d0c
huangxianshuai <huangxianshuai@360.cn>
2018-11-29 17:01:09 +0800

    init

---
+++ QiModularization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QiModularization
+//
+//  Created by huangxianshuai on 2018/11/29.
+//  Copyright © 2018年 QiShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QiModularization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QiModularization
+//
+//  Created by huangxianshuai on 2018/11/29.
+//  Copyright © 2018年 QiShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
